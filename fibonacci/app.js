adaff5f2c11d25e986a7029750d6bdc93083fbbd
Roseau <asboyo@gmail.com>
2018-07-25 09:59:19 +0700

    simple fibonacci app

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//module fibonacci akan diexport dan disimpan kedalam variable fibonacciRouter
+var fibonacciRouter = require('./routes/fibonacci');
 
 //variabel app untuk menampung express
 var app = express();
@@ -28,8 +29,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+//dengan ini, apabila url menggunakan /users, route akan akan diload. dalam kasus ini akan
+//ditampilkan "respond with resources"
+app.use('/fibonacci', fibonacciRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -42,7 +44,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
