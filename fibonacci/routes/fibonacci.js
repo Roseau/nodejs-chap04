6c8c0f004ba7f30885edc5f05e94569e466fc212
Roseau <asboyo@gmail.com>
2018-07-31 09:20:45 +0700

    changing the calculation to use fiboserver

--- fibonacci/routes/fibonacci.js
+++ fibonacci/routes/fibonacci.js
@@ -5,13 +5,25 @@ var router = express.Router();
 var math = require('../math');
 router.get('/', function(req,res,next){
     if(req.query.fibonum){
-        math.fibonacciAsync(req.query.fibonum, (err,fiboval) => {
-            res.render('fibonacci',{
-                title:"Calculate Fibonacci Numbers",
-                fibonum: req.query.fibonum,
-                fiboval: fiboval
+        var httpreq = require('http').request({
+            host:"localhost",
+            port: 3002,
+            path: "/fibonacci/"+Math.floor(req.query.fibonum),
+            method:"GET"
+        }, 
+        httpresp =>{
+            httpresp.on('data',chunk=>{
+                var data = JSON.parse(chunk);
+                res.render('fibonacci',{
+                    title:"Calculate Fibonacci Numbers",
+                    fibonum: req.query.fibonum,
+                    fiboval: data.result
+                });
             });
+            httpresp.on('error', err =>{next(err);});
         });
+        httpreq.on('error', err =>{next(err);});
+        httpreq.end();
     } else {
         res.render('fibonacci',{
             title:"Calculate Fibonacci Numbers",
